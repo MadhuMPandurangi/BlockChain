05df8684cd9ad742eb3960edcc9431f67bfab9fe
MadhuMPandurangi <madhumpandurangi@gmail.com>
2019-03-03 22:22:13 +0530

    Add exixting file

---
+++ lottery-react/src/lottery.js
@@ -0,0 +1,8 @@
+import Web3 from './web3';
+
+const address = '0xBf54CcC660d966A80291C461cF5EfAF889801c97';
+
+
+const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]
+
+export default new Web3.eth.Contract(abi, address);
